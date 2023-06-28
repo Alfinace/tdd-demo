1eca3f101cac6115c393073acd5bc97d92102400
Alfinace <alfinace.rafanomezantsoan@mgenesysinnov.com>
2023-06-28 14:47:06 +0300

    test

--- GameTests/UnitTest.cs
+++ GameTests/UnitTest.cs
@@ -3,12 +3,12 @@ using System;
 
 namespace Game;
 
-public class UnitTest
+public class UnitTest1
 {
     [Fact]
     public void TestClassFighter()
     {
-		Person.ResetCount();
+		Fighter.ResetCount();
 		var fighter = new Fighter("John", "Doe",2001, Level.Easy);
 		var fullName = fighter.GetFullName();
 		// Assert
@@ -19,19 +19,19 @@ public class UnitTest
 	[Fact]
 	public void TestClassCountFighter()
 	{
-		Person.ResetCount();
+		Fighter.ResetCount();
 		var fighter1 = new Fighter("John", "Doe", 2005, Level.Easy);
 		var fighter2 = new Fighter("Ninja", "Dragon", 2004, Level.Medium);
 		var fighter3 = new Fighter("Shadow", "Fighter", 2001, Level.Hard);
 		var fighter4 = new Fighter("Young", "Peter", 1998, Level.Easy);
-		Assert.Equal(4, Person.GetCount());
+		Assert.Equal(4, Fighter.GetCount());
 	}
 
 
 	[Fact]
 	public void TestClassInitialLiveCount()
 	{
-		Person.ResetCount();
+		Fighter.ResetCount();
 		var fighter = new Fighter("John", "Doe", 2001,Level.Easy);
 		Assert.Equal(100, fighter.GetLiveCount());
 	}
